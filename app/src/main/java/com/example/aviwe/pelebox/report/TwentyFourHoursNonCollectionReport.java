e05800044c81ef9c5490f785c75cc0be52ded594
Tharollo <morabatharollo@gmail.com>
2018-12-06 10:20:12 +0200

    Tharollo Adding undefined

--- app/src/main/java/com/example/aviwe/pelebox/report/TwentyFourHoursNonCollectionReport.java
+++ app/src/main/java/com/example/aviwe/pelebox/report/TwentyFourHoursNonCollectionReport.java
@@ -95,7 +95,7 @@ public class TwentyFourHoursNonCollectionReport extends AppCompatActivity implem
             ageString = "65";
         } else if (radioButton.getText().equals("Above 65")) {
             ageString = "A65";
-        } else if (radioButton.getText().equals("All")) {
+        } else if (radioButton.getText().equals("All Age")) {
             ageString = "AA";
 
         } else if (radioButton.getText().equals("Undefined")) {
